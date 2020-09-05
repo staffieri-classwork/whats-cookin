fa865769ccd4b6a39eb8df99c9d7328e57ba3dd0
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-05 15:08:12 -0600

    Chagnege the test files to arrow funtion

--- test/recipe-test.js
+++ test/recipe-test.js
@@ -12,27 +12,27 @@ describe('Recipe', () => {
     recipe = new Recipe(recipeInfo);
   });
 
-  it('is a function', function() {
+  it('is a function', () => {
     expect(Recipe).to.be.a('function');
   });
 
-  it('should be an instance of Recipe', function() {
+  it('should be an instance of Recipe', () => {
     expect(recipe).to.be.an.instanceof(Recipe);
   });
 
-  it('should initialize with an id', function() {
+  it('should initialize with an id', () => {
     expect(recipe.id).to.eq(595736);
   });
 
-  it('should initialize with an name', function() {
+  it('should initialize with an name', () => {
     expect(recipe.name).to.eq('Loaded Chocolate Chip Pudding Cookie Cups');
   });
 
-  it('should initialize with an image', function() {
+  it('should initialize with an image', () => {
     expect(recipe.image).to.eq('https://spoonacular.com/recipeImages/595736-556x370.jpg');
   });
 
-  it('should initialize with an array of ingredients', function() {
+  it('should initialize with an array of ingredients', () => {
     const ingredient = {
       "id": 20081,
       "name": "all purpose flour",
@@ -44,7 +44,7 @@ describe('Recipe', () => {
     expect(recipe.ingredients[0]).to.deep.eq(ingredient);
   });
 
-  it('should calculate the total cost of all of the ingredients', function() {
+  it('should calculate the total cost of all of the ingredients', () => {
     expect(recipe.calculateIngredientsCost()).to.eq();
   });
 });
