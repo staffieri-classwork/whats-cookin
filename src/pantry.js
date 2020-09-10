b59415e7dc2f8463310757cbb4a49550194ba637
Michael Walker <Michael.e.walker.87@gmail.com>
2020-09-09 18:58:41 -0600

    Add the checkIngredients method

--- src/pantry.js
+++ src/pantry.js
@@ -3,19 +3,24 @@ class Pantry {
     this.ingredients = user.pantry;
   }
 
-  checkIngredients(user, recipe) {
-    // console.log(user.pantry, recipe.ingredients)
-    //so we have the stuff we need
-    recipe.ingredients.forEach(pantryItem => {
-      console.log(pantryItem.id)
-    })
-  }
+checkIngredients(user, recipe) {
+  let ingredientsNeeded = recipe.ingredients.reduce((ingredientsNeeded ,ingredientForRecipe) =>{
+    let amountNeeded = ingredientForRecipe.quantity.amount
+    let idNeeded = ingredientForRecipe.id
+    let pantryInventory = user.pantry
+    let item = pantryInventory.find((item) => {
+      return idNeeded === item.ingredient
+    });
+    if(item === undefined || amountNeeded > item.amount){
+      let amountOnHand = item && item.amount || 0
+       ingredientsNeeded.push({name: ingredientForRecipe.name, amountNeeded: ingredientForRecipe.quantity.amount - amountOnHand})
+    }
+    return ingredientsNeeded
+  }, [])
+  return ingredientsNeeded
 }
 
-
-
-
-
+}
 
 
 module.exports = Pantry;
