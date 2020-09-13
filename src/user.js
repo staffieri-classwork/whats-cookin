8fe4a473e59e2b47703b61262a22f1a540f13ba2
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-13 13:47:00 -0700

    Tenatively move functionality from scripts.js to class

--- src/user.js
+++ src/user.js
@@ -18,6 +18,23 @@ class User {
     this.favoriteRecipes.splice(i, 1);
   }
 
+  // favouriteRecipe() {
+  //   if (event.target.className === "card-apple-icon") {
+  //       let cardId = parseInt(event.target.closest(".recipe-card").id)
+  //       if (!this.favoriteRecipes.includes(cardId)) {
+  //         event.target.src = "../images/apple-logo.png";//invoke DOM function
+  //         this.saveRecipe(cardId);
+  //       } else {
+  //         event.target.src = "../images/apple-logo-outline.png"; //invoke DOM function
+  //         this.removeRecipe(cardId);
+  //       }
+  //     } else if (event.target.id === "exit-recipe-btn") {
+  //       exitRecipe();
+  //     } else if (isDescendant(event.target.closest(".recipe-card"), event.target)) {
+  //       openRecipeInfo(event);
+  //     }
+  // } can. can you add addEventListeners to class methods. i actually. have no idea??
+
   decideToCook(recipe) {
     this.recipesToCook.push(recipe);
   }
