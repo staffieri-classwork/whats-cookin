00163950247bd42bd999988398eaf273cfc49940
Brigette Doelp <brigette.doelp@gmail.com>
2020-09-16 16:42:10 -0700

    Remove comments, console.logs, adjust tabbing

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -13,113 +13,38 @@ let domUpdates = {
       <h4>${recipeInfo.tags[0]}</h4>
       <img src="../images/apple-logo-outline.png" alt="unfilled apple icon" class="card-apple-icon">
     </article>`
-  main.insertAdjacentHTML("beforeend", cardHtml);
-},
+    main.insertAdjacentHTML("beforeend", cardHtml);
+  },
 
-capitalize(words) {
-  return words.split(" ").map(word => {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-  }).join(" ");
-},
+  capitalize(words) {
+    return words.split(" ").map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  },
 
- listTags(allTags) {
-  let tagList = document.querySelector(".tag-list");
-  allTags.forEach(tag => {
-    let tagHtml = `<li><input type="checkbox" class="checked-tag" id="${tag}ID">
-      <label for="${tag}">${this.capitalize(tag)}</label></li>`;
-    tagList.insertAdjacentHTML("beforeend", tagHtml);
-  });
-},
+  showMyRecipesBanner() {
+    document.querySelector(".welcome-msg").style.display = "none";
+    document.querySelector(".my-recipes-banner").style.display = "block";
+  },
 
- hideUnselectedRecipes(foundRecipes) {
-  foundRecipes.forEach(recipe => {
-    let domRecipe = document.getElementById(`${recipe.id}`);
-    domRecipe.style.display = "none";
-  });
-},
+  showWelcomeBanner() {
+    document.querySelector(".welcome-msg").style.display = "flex";
+    document.querySelector(".my-recipes-banner").style.display = "none";
+  },
 
- openRecipeInfo(event, data, ingredientData) {
-  let fullRecipeInfo = document.querySelector(".recipe-instructions");
-  fullRecipeInfo.style.display = "inline";
-  let recipeId = event.path.find(e => e.id).id;
-  let recipe = data.find(recipe => recipe.id === Number(recipeId));
-  this.generateRecipeTitle(recipe, this.generateIngredients(recipe, ingredientData));
-  this.addRecipeImage(recipe);
-  this.generateInstructions(recipe);
-  fullRecipeInfo.insertAdjacentHTML("beforebegin", "<section id='overlay'></div>");
-},
+  addRecipeImage(recipe) {
+    document.getElementById("recipe-title").style.backgroundImage = `url(${recipe.image})`;
+  },
 
-generateInstructions(recipe) {
-  let fullRecipeInfo = document.querySelector(".recipe-instructions");
-  let instructionsList = "";
-  let instructions = recipe.instructions.map(i => {
-    return i.instruction
-  });
-  instructions.forEach(i => {
-    instructionsList += `<li>${i}</li>`
-  });
-  fullRecipeInfo.insertAdjacentHTML("beforeend", "<h4>Instructions</h4>");
-  fullRecipeInfo.insertAdjacentHTML("beforeend", `<ol>${instructionsList}</ol>`);
-},
-
- generateRecipeTitle(recipe, ingredients) {
-  let fullRecipeInfo = document.querySelector(".recipe-instructions");
-  let recipeTitle = `
-    <button id="exit-recipe-btn">X</button>
-    <h3 id="recipe-title">${recipe.name}</h3>
-    <h4>Ingredients</h4>
-    <p>${ingredients}</p>`
-  fullRecipeInfo.insertAdjacentHTML("beforeend", recipeTitle);
-},
-
-generateIngredients(recipe, ingredientData) {
-  return recipe && recipe.ingredients.map(i => {
-    let ingredientNeeded = ingredientData.find((ingredient) => {
-      return i.id === ingredient.id
-    })
-    return `${ingredientNeeded.name} (${i.quantity.amount} ${i.quantity.unit})`
-  }).join(", ");
-},
-
- addRecipeImage(recipe) {
-  document.getElementById("recipe-title").style.backgroundImage = `url(${recipe.image})`;
-},
-
- exitRecipe() {
-  let fullRecipeInfo = document.querySelector(".recipe-instructions");
-  while (fullRecipeInfo.firstChild &&
-    fullRecipeInfo.removeChild(fullRecipeInfo.firstChild));
-  fullRecipeInfo.style.display = "none";
-  document.getElementById("overlay").remove();
-},
-
- showMyRecipesBanner() {
-  document.querySelector(".welcome-msg").style.display = "none";
-  document.querySelector(".my-recipes-banner").style.display = "block";
-},
-
- showWelcomeBanner() {
-  document.querySelector(".welcome-msg").style.display = "flex";
-  document.querySelector(".my-recipes-banner").style.display = "none";
-},
-
- toggleMenu() {
-  var menuDropdown = document.querySelector(".drop-menu");
-  menuOpen = !menuOpen;
-  if (menuOpen) {
-    menuDropdown.style.display = "block";
-  } else {
-    menuDropdown.style.display = "none";
-  }
-},
-
- showAllRecipes() {
-  recipes.forEach(recipe => {
-    let domRecipe = document.getElementById(`${recipe.id}`);
-    domRecipe.style.display = "block";
-  });
-  this.showWelcomeBanner();
-},
+   toggleMenu() {
+    var menuDropdown = document.querySelector(".drop-menu");
+    menuOpen = !menuOpen;
+    if (menuOpen) {
+      menuDropdown.style.display = "block";
+    } else {
+      menuDropdown.style.display = "none";
+    }
+  },
 
   displayPantryInfo(pantry) {
     pantry.forEach(ingredient => {
@@ -130,6 +55,80 @@ generateIngredients(recipe, ingredientData) {
     });
   },
 
+  showAllRecipes() {
+   recipes.forEach(recipe => {
+     let domRecipe = document.getElementById(`${recipe.id}`);
+     domRecipe.style.display = "block";
+   });
+   this.showWelcomeBanner();
+ },
+
+  listTags(allTags) {
+    let tagList = document.querySelector(".tag-list");
+    allTags.forEach(tag => {
+      let tagHtml = `<li><input type="checkbox" class="checked-tag" id="${tag}ID">
+      <label for="${tag}">${this.capitalize(tag)}</label></li>`;
+      tagList.insertAdjacentHTML("beforeend", tagHtml);
+    });
+  },
+
+  hideUnselectedRecipes(foundRecipes) {
+    foundRecipes.forEach(recipe => {
+      let domRecipe = document.getElementById(`${recipe.id}`);
+      domRecipe.style.display = "none";
+    });
+  },
+
+  openRecipeInfo(event, data, ingredientData) {
+    let fullRecipeInfo = document.querySelector(".recipe-instructions");
+    fullRecipeInfo.style.display = "inline";
+    let recipeId = event.path.find(e => e.id).id;
+    let recipe = data.find(recipe => recipe.id === Number(recipeId));
+    this.generateRecipeTitle(recipe, this.generateIngredients(recipe, ingredientData));
+    this.addRecipeImage(recipe);
+    this.generateInstructions(recipe);
+    fullRecipeInfo.insertAdjacentHTML("beforebegin", "<section id='overlay'></div>");
+  },
+
+  generateRecipeTitle(recipe, ingredients) {
+    let fullRecipeInfo = document.querySelector(".recipe-instructions");
+    let recipeTitle = `
+    <button id="exit-recipe-btn">X</button>
+    <h3 id="recipe-title">${recipe.name}</h3>
+    <h4>Ingredients</h4>
+    <p>${ingredients}</p>`
+    fullRecipeInfo.insertAdjacentHTML("beforeend", recipeTitle);
+  },
+
+  generateInstructions(recipe) {
+    let fullRecipeInfo = document.querySelector(".recipe-instructions");
+    let instructionsList = "";
+    let instructions = recipe.instructions.map(i => {
+      return i.instruction
+    });
+    instructions.forEach(i => {
+      instructionsList += `<li>${i}</li>`
+    });
+    fullRecipeInfo.insertAdjacentHTML("beforeend", "<h4>Instructions</h4>");
+    fullRecipeInfo.insertAdjacentHTML("beforeend", `<ol>${instructionsList}</ol>`);
+  },
+
+  generateIngredients(recipe, ingredientData) {
+    return recipe && recipe.ingredients.map(i => {
+      let ingredientNeeded = ingredientData.find((ingredient) => {
+        return i.id === ingredient.id
+      })
+      return `${ingredientNeeded.name} (${i.quantity.amount} ${i.quantity.unit})`
+    }).join(", ");
+  },
+
+   exitRecipe() {
+    let fullRecipeInfo = document.querySelector(".recipe-instructions");
+    while (fullRecipeInfo.firstChild &&
+      fullRecipeInfo.removeChild(fullRecipeInfo.firstChild));
+    fullRecipeInfo.style.display = "none";
+    document.getElementById("overlay").remove();
+  },
 }
 
 export default domUpdates;
--- src/pantry.js
+++ src/pantry.js
@@ -4,50 +4,45 @@ class Pantry {
     this.ingredientsData = ingredientsData;
   }
 
-checkIngredients(user, recipe) {
-  let ingredientsNeeded = recipe.ingredients.reduce((ingredientsNeeded , ingredientForRecipe) =>{
-    let amountNeeded = ingredientForRecipe.quantity.amount
-    let idNeeded = ingredientForRecipe.id
-    let pantryInventory = user.pantry
-    let item = pantryInventory.find((item) => {
-      return idNeeded === item.ingredient
-    });
-    if (item === undefined || amountNeeded > item.amount){
-      let amountOnHand = item && item.amount || 0
-      let needAmount = ingredientForRecipe.quantity.amount - amountOnHand
-       ingredientsNeeded.push(
-         {name: ingredientForRecipe.name,
-           amountNeeded: needAmount,
-           id: ingredientForRecipe.id,
-           unitsNeeded: ingredientForRecipe.quantity.unit,
-         })
-    }
+  checkIngredients(user, recipe) {
+    let ingredientsNeeded = recipe.ingredients.reduce((ingredientsNeeded , ingredientForRecipe) =>{
+      let amountNeeded = ingredientForRecipe.quantity.amount
+      let idNeeded = ingredientForRecipe.id
+      let pantryInventory = user.pantry
+      let item = pantryInventory.find((item) => {
+        return idNeeded === item.ingredient
+      });
+      if (item === undefined || amountNeeded > item.amount){
+        let amountOnHand = item && item.amount || 0
+        let needAmount = ingredientForRecipe.quantity.amount - amountOnHand
+         ingredientsNeeded.push(
+           {name: ingredientForRecipe.name,
+             amountNeeded: needAmount,
+             id: ingredientForRecipe.id,
+             unitsNeeded: ingredientForRecipe.quantity.unit,
+           })
+      }
+      return ingredientsNeeded
+    }, [])
     return ingredientsNeeded
-  }, [])
-  return ingredientsNeeded
-}
+  }
 
-determineCost(user, recipe) {
-  let neededIngredient = this.checkIngredients(user, recipe)
-  let estimatedCostNeeded = neededIngredient.reduce((costNeeded, item) => {
-    let lookedUpItem = this.ingredientsData.find((ingredient) => {
-      return ingredient.id === item.id
-    })
-    if(neededIngredient !== []) {
-      let itemCost = (item.amountNeeded * lookedUpItem.estimatedCostInCents) / 100
-      costNeeded.push(`We need to get ${item.amountNeeded} more ${item.unitsNeeded} ${item.name} that will roughly cost $${itemCost} for the recipe`)
-    } else if (costNeeded === []) {
-      costNeeded.push("We have the ingredients to cook the recipe")
-    }
-    return costNeeded
-  }, [])
-  return estimatedCostNeeded.join('; also ')
-}
-
-// addNecessay(user, recipe){
-//
-// }
-
+  determineCost(user, recipe) {
+    let neededIngredient = this.checkIngredients(user, recipe)
+    let estimatedCostNeeded = neededIngredient.reduce((costNeeded, item) => {
+      let lookedUpItem = this.ingredientsData.find((ingredient) => {
+        return ingredient.id === item.id
+      })
+      if(neededIngredient !== []) {
+        let itemCost = (item.amountNeeded * lookedUpItem.estimatedCostInCents) / 100
+        costNeeded.push(`We need to get ${item.amountNeeded} more ${item.unitsNeeded} ${item.name} that will roughly cost $${itemCost} for the recipe`)
+      } else if (costNeeded === []) {
+        costNeeded.push("We have the ingredients to cook the recipe")
+      }
+      return costNeeded
+    }, [])
+    return estimatedCostNeeded.join('; also ')
+  }
 }
 
 
